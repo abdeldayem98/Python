b91b2d55388f562bed862a500fa6a767c5f74d84
abdeldayem98 <114102329+abdeldayem98@users.noreply.github.com>
2023-03-27 23:30:14 -0400

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -1,6 +1,6 @@
 ##This python program encrypts a message from user input based on a specified distance value; using the cipher technique.
 ##Marwan Abdeladyem 3/27/2023
-
+##1.0
 message = input("Enter a message: ")
 distanceValue = int(input("Enter distance value: "))
 result = ""
